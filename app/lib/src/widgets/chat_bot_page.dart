3098130896c98f98d1fa8bb1b61c4821cade2775
Mahdi Barhoumi <mehdi192001@yahoo.fr>
2024-04-27 11:47:18 +0100

    Worked on things.

--- app/lib/src/widgets/chat_bot_page.dart
+++ app/lib/src/widgets/chat_bot_page.dart
@@ -16,7 +16,8 @@ class ChatBotPageState extends State<ChatBotPage> {
   final messageController = TextEditingController();
   late final HttpClient client;
   List<dynamic> conversation = [];
-  var error = false;
+  bool isLoading = true;
+  bool error = false;
 
   ChatBotPageState() {
     final securityContext = SecurityContext();
@@ -39,53 +40,104 @@ class ChatBotPageState extends State<ChatBotPage> {
   }
 
   Future<void> getConversation() async {
-    final token = await FirebaseAuth.instance.currentUser!.getIdToken();
-    final request = await client.getUrl(Uri.parse('https://skindiagnostics.ddns.net/chatbot/getConversation'));
-    request.headers.set('token', token!);
-    final response = await request.close();
-    if (response.statusCode == HttpStatus.ok) {
-      final jsonString = await response.transform(utf8.decoder).join();
+    try {
       setState(() {
-        conversation = jsonDecode(jsonString) as List<dynamic>;
+        isLoading = true;
       });
-      if (conversation.isEmpty) {
-        generateResponse();
+      final token = await FirebaseAuth.instance.currentUser!.getIdToken();
+      final request = await client.getUrl(Uri.parse('https://skindiagnostics.ddns.net/chatbot/getConversation'));
+      request.headers.set('token', token!);
+      final response = await request.close();
+      if (response.statusCode == HttpStatus.ok) {
+        final jsonString = await response.transform(utf8.decoder).join();
+        setState(() {
+          conversation = jsonDecode(jsonString) as List<dynamic>;
+          isLoading = false;
+          error = false;
+        });
+        if (conversation.isEmpty) {
+          generateResponse();
+        }
+      } else {
+        setState(() {
+          isLoading = false;
+          error = true;
+        });
       }
+    } on Exception {
+      setState(() {
+        isLoading = false;
+        error = true;
+      });
     }
   }
 
   Future<void> generateResponse() async {
-    final token = await FirebaseAuth.instance.currentUser!.getIdToken();
-    final request = await client.getUrl(Uri.parse('https://skindiagnostics.ddns.net/chatbot/generateResponse'));
-    request.headers.set('token', token!);
-    final response = await request.close();
-    if (response.statusCode == 200) {
-      conversation.add({'role': 'assistant', 'content': ''});
-      await for (var text in response.transform(utf8.decoder)) {
+    try {
+      final token = await FirebaseAuth.instance.currentUser!.getIdToken();
+      final request = await client.getUrl(Uri.parse('https://skindiagnostics.ddns.net/chatbot/generateResponse'));
+      request.headers.set('token', token!);
+      final response = await request.close();
+      if (response.statusCode == 200) {
+        conversation.add({'role': 'assistant', 'content': ''});
+        await for (var text in response.transform(utf8.decoder)) {
+          setState(() {
+            conversation.last['content'] += text;
+          });
+        }
+      } else {
         setState(() {
-          conversation.last['content'] += text;
+          error = true;
         });
       }
-      getConversation();
+    } on Exception {
+      setState(() {
+        error = true;
+      });
     }
   }
 
   Future<void> deleteConversation() async {
-    final token = await FirebaseAuth.instance.currentUser!.getIdToken();
-    final request = await client.deleteUrl(Uri.parse('https://skindiagnostics.ddns.net/chatbot/deleteConversation'));
-    request.headers.set('token', token!);
-    await request.close();
-    getConversation();
+    try {
+      final token = await FirebaseAuth.instance.currentUser!.getIdToken();
+      final request = await client.deleteUrl(Uri.parse('https://skindiagnostics.ddns.net/chatbot/deleteConversation'));
+      request.headers.set('token', token!);
+      final response = await request.close();
+      if (response.statusCode != 200) {
+        setState(() {
+          error = true;
+        });
+      }
+    } on Exception {
+      setState(() {
+        error = true;
+      });
+    }
   }
 
   Future<void> addMessage(String? message) async {
-    if (message != null && message.isNotEmpty) {
-      final token = await FirebaseAuth.instance.currentUser!.getIdToken();
-      final request = await client.postUrl(Uri.parse('https://skindiagnostics.ddns.net/chatbot/addMessage'));
-      request.headers.set('token', token!);
-      request.headers.set('message', message);
-      await request.close();
-      getConversation();
+    try {
+      if (message != null && message.isNotEmpty) {
+        final token = await FirebaseAuth.instance.currentUser!.getIdToken();
+        final request = await client.postUrl(Uri.parse('https://skindiagnostics.ddns.net/chatbot/addMessage'));
+        request.headers.set('token', token!);
+        request.headers.set('message', message);
+        final response = await request.close();
+        if (response.statusCode == HttpStatus.ok) {
+          setState(() {
+            conversation.add({'role': 'user', 'content': message});
+          });
+          generateResponse();
+        } else {
+          setState(() {
+            error = true;
+          });
+        }
+      }
+    } on Exception {
+      setState(() {
+        error = true;
+      });
     }
   }
 
@@ -100,7 +152,7 @@ class ChatBotPageState extends State<ChatBotPage> {
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: IconButton(
               icon: const Icon(Icons.restart_alt_rounded),
-              onPressed: deleteConversation,
+              onPressed: getConversation,
             ),
           )
         ],
@@ -110,50 +162,60 @@ class ChatBotPageState extends State<ChatBotPage> {
         children: [
           Expanded(
             flex: 9,
-            child: ListView.builder(
-              reverse: true,
-              itemCount: conversation.length,
-              itemBuilder: (context, index) {
-                index = conversation.length - 1 - index;
-                if (conversation[index]['role'] == 'user') {
-                  return Container(
-                    margin: const EdgeInsets.only(right: 20, left: 75, top: 10, bottom: 10),
-                    alignment: Alignment.centerRight,
-                    child: Container(
-                      padding: const EdgeInsets.all(12.5),
-                      decoration: BoxDecoration(
-                        boxShadow: const [BoxShadow(color: Colors.grey, blurRadius: 5)],
-                        color: darken(Theme.of(context).colorScheme.surface, percentage: 0.01),
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        ),
-                      ),
-                      child: Text(
-                        conversation[index]['content'],
-                        style: const TextStyle(fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                  );
+            child: Builder(
+              builder: (context) {
+                if (isLoading) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (error) {
+                  return const Center(child: Text('An error has occurred, please reload the page.'));
                 } else {
-                  return Container(
-                    margin: const EdgeInsets.only(right: 75, left: 20, top: 10, bottom: 10),
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      padding: const EdgeInsets.all(12.5),
-                      decoration: BoxDecoration(
-                        boxShadow: const [BoxShadow(color: Colors.grey, blurRadius: 5)],
-                        color: Theme.of(context).colorScheme.primary,
-                        borderRadius: const BorderRadius.only(
-                          topRight: Radius.circular(10),
-                          bottomLeft: Radius.circular(10),
-                        ),
-                      ),
-                      child: Text(
-                        conversation[index]['content'],
-                        style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
-                      ),
-                    ),
+                  return ListView.builder(
+                    reverse: true,
+                    itemCount: conversation.length,
+                    itemBuilder: (context, index) {
+                      index = conversation.length - 1 - index;
+                      if (conversation[index]['role'] == 'user') {
+                        return Container(
+                          margin: const EdgeInsets.only(right: 20, left: 75, top: 10, bottom: 10),
+                          alignment: Alignment.centerRight,
+                          child: Container(
+                            padding: const EdgeInsets.all(12.5),
+                            decoration: BoxDecoration(
+                              boxShadow: const [BoxShadow(color: Colors.grey, blurRadius: 5)],
+                              color: darken(Theme.of(context).colorScheme.surface, percentage: 0.01),
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              conversation[index]['content'],
+                              style: const TextStyle(fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        );
+                      } else {
+                        return Container(
+                          margin: const EdgeInsets.only(right: 75, left: 20, top: 10, bottom: 10),
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                            padding: const EdgeInsets.all(12.5),
+                            decoration: BoxDecoration(
+                              boxShadow: const [BoxShadow(color: Colors.grey, blurRadius: 5)],
+                              color: Theme.of(context).colorScheme.primary,
+                              borderRadius: const BorderRadius.only(
+                                topRight: Radius.circular(10),
+                                bottomLeft: Radius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              conversation[index]['content'],
+                              style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        );
+                      }
+                    },
                   );
                 }
               },
@@ -166,9 +228,22 @@ class ChatBotPageState extends State<ChatBotPage> {
               boxShadow: [BoxShadow(color: Colors.grey.shade500, blurRadius: 5)],
             ),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
-                  flex: 8,
+                  flex: 1,
+                  child: IconButton(
+                    icon: const Icon(Icons.add_comment_outlined),
+                    onPressed: () async {
+                      if (!error) {
+                        await deleteConversation();
+                        getConversation();
+                      }
+                    },
+                  ),
+                ),
+                Expanded(
+                  flex: 7,
                   child: Padding(
                     padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
                     child: TextField(
@@ -178,10 +253,11 @@ class ChatBotPageState extends State<ChatBotPage> {
                         border: OutlineInputBorder(borderRadius: BorderRadius.circular(50)),
                       ),
                       controller: messageController,
-                      onSubmitted: (message) async {
-                        messageController.clear();
-                        await addMessage(message);
-                        generateResponse();
+                      onSubmitted: (message) {
+                        if (!error) {
+                          addMessage(messageController.text);
+                          messageController.clear();
+                        }
                       },
                     ),
                   ),
@@ -190,10 +266,11 @@ class ChatBotPageState extends State<ChatBotPage> {
                   flex: 1,
                   child: IconButton(
                     icon: const Icon(Icons.send),
-                    onPressed: () async {
-                      messageController.clear();
-                      await addMessage(messageController.text);
-                      generateResponse();
+                    onPressed: () {
+                      if (!error) {
+                        addMessage(messageController.text);
+                        messageController.clear();
+                      }
                     },
                   ),
                 )
